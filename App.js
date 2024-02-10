b91b6e52a45090626e0867d5422438c0cbe1c3f9
anddev68 <chk392013@gmail.com>
2024-02-10 22:10:51 +0900

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { RadioButton, Text, TextInput } from 'react-native-paper';
 
 export default function App() {
+  // 割る数
+  const [divisor, setDivisor] = useState(23)
+  // 割られるかず
+  const [dividend, setDividend] = useState("0")
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text variant='titleLarge'>割られる数</Text>
+      <TextInput
+        value={dividend}
+        inputMode='numeric'
+        onChangeText={(text) => {
+          setDividend(text)
+        }}
+      />
+
+      <Text variant='titleLarge'>割る数</Text>
+      <Text>23</Text>
+      <RadioButton
+        value={23}
+        status={divisor === 23 ? "checked": "unchecked"}
+        onPress={() => { setDivisor(23) }}
+      />
+      <Text>46</Text>
+      <RadioButton
+        value={46}
+        status={divisor === 46 ? "checked": "unchecked"}
+        onPress={() => { setDivisor(46) }}
+      />
+      <Text>50</Text>
+      <RadioButton
+        value={50}
+        status={divisor === 50 ? "checked": "unchecked"}
+        onPress={() => { setDivisor(50)}}
+      />
+
+      <Text variant='titleLarge'>結果</Text>
+      <Text>{ Math.floor(Number(dividend) / divisor) } あまり { Number(dividend) % divisor }</Text>
     </View>
   );
 }
